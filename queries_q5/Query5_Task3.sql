da77913b8c130904db45f086ac7bec94ea6e857b
Matt Romanes <48772682+romanesmatt@users.noreply.github.com>
2022-04-26 22:52:42 +1200

    Complete Q6, separate my and mom's solutions

--- queries_q5/Query5_Task3.sql
+++ queries_q5/Query5_Task3.sql
@@ -1,6 +1,4 @@
-Select DISTINCT r.RobberId,r.NickName,s.Description
-From Robbers r NATURAL JOIN Skills s NATURAL JOIN HasSkills hs Natural JOIN
-(Select r1.RobberId,r1.NickName,count(r1.RobberId) AS c
-From Robbers r1 NATURAL JOIN Skills s1 NATURAL JOIN HasSkills hs1
-GROUP BY r1.RobberId) c1
-WHERE c1.c>=2
+SELECT r.RobberId, r.Nickname, s.Description
+FROM Robbers r NATURAL JOIN HasSkills h NATURAL JOIN Skills s
+GROUP BY r.RobberId, r.Nickname, h.Preference, s.Description
+HAVING h.Preference = 1;
--- queries_q6/Query6_Task2.sql
+++ queries_q6/Query6_Task2.sql
@@ -1,66 +1,22 @@
-
-
--- Mom's Solution
--- Question 6.2
--- no of robberies per security  level
-create view robberiespersec as 
-select banks.security, count(bankrobberies.norob) from banks banks
-right join 
-(select bankname, city, count(*) as norob from robberies
-group by bankname,city
-) bankrobberies on bankrobberies.bankname = banks.bankname and bankrobberies.city=banks.city
-group by banks.security
-
--- average amount 
-create view avgamt as
-select banks.security, avg (bankrobberies.amount) aveamt from banks banks
-inner join 
-(select bankname,  amount as amount from robberies
-group by bankname,amount
-) bankrobberies on bankrobberies.bankname = banks.bankname
-group by banks.security
-
-
--- join
-create view securitylevelsandamounts as
-select banks.security, count(bankrobberies.norob),avg(bankrobberies2.amount) from banks banks
-inner join(select bankname, city, count(*) as norob from robberies
-group by bankname, city
-) bankrobberies on bankrobberies.bankname = banks.bankname
-inner join (select bankname, city, amount as amount from robberies
-group by bankname,city,amount
-) bankrobberies2 on bankrobberies2.bankname = banks.bankname
-group by banks.security
-
-select * from bank
-where bankname not in (select bankname from robberies)
-
-select security from banks
-where 
-
---no of robberies per bank
-select bankname, count(*) from robberies
-group by bankname
-
 -- My solution
 -- Step wise approach
-CREATE VIEW securityAmt as (
+CREATE VIEW SecurityAmount as (
 SELECT b.BankName, b.City, b.Security, r.Amount
 FROM Banks b NATURAL JOIN Robberies r
 ORDER BY b.Security);
 
-CREATE VIEW robberAmt as (
-SELECT Security, COUNT(Security) AS NumberRobberies, ROUND(AVG(Amount),2 ) AS
-AverageAmount
-FROM securityAmt
+CREATE VIEW RobberAmount as (
+SELECT Security, COUNT(Security) AS NumberRobberies, 
+AVG(Amount) AS AverageAmount
+FROM SecurityAmount
 GROUP BY Security
 ORDER BY NumberRobberies DESC);
  
 --  Nested query
 SELECT Security AS SecurityLevel, COUNT(Security) AS NumberRobberies,
-ROUND(AVG(Amount), 2) AS AverageAmount
+AVG(Amount) AS AverageAmount
 FROM (SELECT b.BankName, b.City, b.Security, r.Amount
-FROM Robberies r NATURAL JOIN Bank b) AS sec
+FROM Robberies r NATURAL JOIN Banks b) AS sec
 GROUP BY Security
 ORDER BY NumberRobberies DESC
 
