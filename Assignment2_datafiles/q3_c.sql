d9d880f42c1c7c3c06d0f3d8bf3fe776c27f5605
Matt Romanes <48772682+romanesmatt@users.noreply.github.com>
2022-06-08 13:33:15 +1200

    Add files via upload

---
+++ Assignment2_datafiles/q3_c.sql
@@ -0,0 +1,40 @@
+-- Original solution
+select clb.f_name, clb.l_name, noofbooks  
+
+from (select f_name, l_name, count(*) as noofbooks  
+
+     from customer natural join loaned_book  
+
+     group by f_name, l_name) as clb  
+
+     where 3 > (select count(*)  
+
+                from (select f_name, l_name, count(*) as noofbooks  
+
+                      from customer natural join loaned_book  
+
+                      group by f_name, l_name) as clb1  
+
+                      where clb.noofbooks<clb1.noofbooks)  
+
+                      order by noofbooks desc; 
+
+
+                    --   My solution
+                    select clb.f_name,
+    clb.l_name,
+    noofbooks
+from (
+        select f_name,
+            l_name,
+            count(*) as noofbooks
+        from customer
+            natural join loaned_book
+        group by f_name,
+            l_name
+    ) as clb
+    
+    where noofbooks >= 3
+    order by noofbooks desc; -- Cost: 3.35
+    -- Number of tuples shown: 15
+
